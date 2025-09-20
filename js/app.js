4c749a482b1664e4151776a67d84a064d99583bd
vioviper <cgiron_17@hotmail.com>
2025-09-20 14:57:40 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,127 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'App Framework7',
+	panel: {
+		swipe: true,
+	},
+  	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/perfil/',
+    	url: 'perfil.html',
+    	name: 'perfil',
+  		},
+		{
+		path: '/preguntas/',
+    	url: 'preguntas.html',
+    	name: 'preguntas',
+  		},
+		{
+		path: '/sobrenosotros/',
+    	url: 'sobrenosotros.html',
+    	name: 'sobrenosotros',
+  		},
+		{
+		path: '/informacion/',
+    	url: 'informacion.html',
+    	name: 'informacion',
+  		},
+		{
+		path: '/detalles/',
+    	url: 'detalles.html',
+    	name: 'detalles',
+  		},
+		{
+		path: '/guardados/',
+    	url: 'guardados.html',
+    	name: 'guardados',
+  		},
+		{
+		path: '/preguntas/',
+    	url: 'preguntas.html',
+    	name: 'preguntas',
+  		},
+	],
+	dialog: {
+		title: 'Puppy Finder',
+		buttonOk: 'Aceptar',
+  	},
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
+
+
+
+$$('#btnLogin').on('click', function (e) {
+	e.preventDefault();
+	
+	var $valid = $$('#form-login')[0].checkValidity();
+	if ($valid){
+        $$('#form-login').trigger('reset');	
+        app.loginScreen.close('#modal-login');
+		
+		app.dialog.alert('¡ Bienvenido a la APP !');
+    }
+	
+});
+
+
+
+$$(document).on('click', '#btnAddToCart', function (e) {
+	e.preventDefault();
+
+	
+	app.dialog.confirm('¿Seguro desea agregar este producto?', function () {
+		
+		//app.dialog.alert('Great!');
+		
+		var notification = app.notification.create({
+		  icon: '<i class="material-icons">check</i>',
+		  title: 'Order',
+		  titleRightText: '',
+		  subtitle: '',
+		  text: "Su producto ha sido agregado al carrito correctamente",
+		  closeTimeout: 3000,
+		});
+		notification.open();
+		
+		//redirect
+		app.views.main.router.navigate('/categories/');
+		
+	});
+	
+	
+
+	
+});
+
+
+
+
+
